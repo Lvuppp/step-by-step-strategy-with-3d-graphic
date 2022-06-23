c86563d1deedf35bacea4e3da2e566996d41a29a
Egor <egor.bagrovets.eb@gmail.com>
2022-06-23 20:48:28 +0300

    addCoreProject

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(){
-
-    std::cout << "Hello world" << std::endl;
-    std::cout << "Add something" << std::endl;
-    std::cout << "ya tyt bil med i pivo pil" << std::endl;
-    return 0;
-}
