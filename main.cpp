906f4e7d4cff18d54e27bb822bfa05022a3a3959
Egor <egor.bagrovets.eb@gmail.com>
2022-06-23 15:51:37 +0300

    sfmlAdd

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ int main(){
 
     std::cout << "Hello world" << std::endl;
     std::cout << "Add something" << std::endl;
-    std::cout << "ya tyt bil med i pivo pil" << endl;
+    std::cout << "ya tyt bil med i pivo pil" << std::endl;
     return 0;
 }
